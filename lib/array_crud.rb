cf8d11ef520bb6a0063de816eb201edbd3e195e6
Catherine Price <catprice444@gmail.com>
2020-04-21 21:21:55 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,7 +28,7 @@ end
 
 def retrieve_element_from_index(array, index_number)
   array = ["wow","cats", "am", "arrays!"]
-  array.index("am")
+  array.index(2)
 end
 
 def retrieve_first_element_from_array(array)
